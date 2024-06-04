36094481fbbf3c86a3068ba776ab738095de4a0e
M.RWEMA REMY <rwemaremy21@gmail.com>
2024-06-04 08:50:22 +0200

    ft-login-with-google-187300097

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+'use client';
+
 import Link from 'next/link';
-import React from 'react';
+import { useSearchParams } from 'next/navigation';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const token = searchParams.get('token');
+  useEffect(() => {
+    if (token) {
+      localStorage.setItem('token', token);
+      router.push('/');
+    }
+  });
+
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <span className="mt-6 mx-auto text-sm text-black">
--- src/components/formElements/ButtonElement.tsx
+++ src/components/formElements/ButtonElement.tsx
@@ -1,4 +1,4 @@
-import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 
 interface ButtonProps {
   label: string;
@@ -9,22 +9,24 @@ interface ButtonProps {
 }
 
 export enum ButtonStyle {
-  LIGHT = "light",
-  DARK = "dark",
+  LIGHT = 'light',
+  DARK = 'dark'
 }
 
 export const ButtonComponent = (props: ButtonProps) => {
   return (
     <div className="relative">
       <button
-        type={"submit"}
+        type={'submit'}
         disabled={props.disabled}
         className={`rounded-full w-max p-3 text-base hover:bg-main-300 hover:shadow-md flex items-center justify-center gap-2 cursor-pointer mt-10 mx-auto  ${
           props.style === ButtonStyle.LIGHT
-            ? "border-1 border-main-400  font-medium text-main-400 bg-transparent"
+            ? 'border-1 border-main-400  font-medium text-main-400 bg-transparent'
             : props.style === ButtonStyle.DARK
-            ? "bg-main-400 text-main-100 font-medium"
-            : props.loading? "animate-ping": ""
+              ? 'bg-main-400 text-main-100 font-medium'
+              : props.loading
+                ? 'animate-ping'
+                : ''
         }`}
       >
         {props.loading ? (
