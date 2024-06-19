3f8f77d0e70e0bac3e6d162a6f1e622e8390b347
Mukunzijames <113775816+Mukunzijames@users.noreply.github.com>
2024-06-19 16:02:46 +0200

    feat(addItem):a seller should create product (#26)
    [#187300103]

--- src/components/formElements/Input.tsx
+++ src/components/formElements/Input.tsx
@@ -6,7 +6,7 @@ interface InputProps {
   label: string;
   placeholder: string;
   onChange: ChangeEventHandler<HTMLInputElement>;
-  value: string;
+  value: string | File[] | undefined;
   valid: boolean;
   type?: React.HTMLInputTypeAttribute;
   min?: number;
@@ -14,23 +14,32 @@ interface InputProps {
   readOnly?: boolean;
 }
 
-export const Input = ({ type = 'text', valid, ...rest }: InputProps) => {
+export const Input = ({ type = 'text', valid, value, ...rest }: InputProps) => {
   const [passwordMode, setPasswordMode] = useState<boolean>(true);
+
+  let displayValue: string;
+  if (Array.isArray(value)) {
+    displayValue = value.map(file => file.name).join(', ');
+  } else {
+    displayValue = value || '';
+  }
 
   return (
     <div className="relative">
       <input
         {...rest}
+        value={displayValue}
         type={type === 'password' && !passwordMode ? 'text' : type || 'text'}
-        className={`border-b-2 placeholder-gray-400 focus:outline-none focus:border-black active:bg-transparent   w-full pt-4 p-2 m-0 text-sm block bg-transparent ${valid ? ' border-gray-400' : 'border-red-600'} text-main-400`}
+        className={`border-b-2 placeholder-gray-400 focus:outline-none focus:border-black active:bg-transparent w-full pt-4 p-2 m-0 text-sm block bg-transparent ${valid ? 'border-gray-400' : 'border-red-600'} text-main-400`}
       />
-      <div
-        onClick={() => setPasswordMode(!passwordMode)}
-        className="absolute right-0 top-4 cursor-pointer hover:scale-x-110 text-main-200 hover:text-main-300"
-      >
-        {type === 'password' &&
-          (passwordMode ? <GoEye size={20} /> : <GoEyeClosed size={20} />)}
-      </div>
+      {type === 'password' && (
+        <div
+          onClick={() => setPasswordMode(!passwordMode)}
+          className="absolute right-0 top-4 cursor-pointer hover:scale-x-110 text-main-200 hover:text-main-300"
+        >
+          {passwordMode ? <GoEye size={20} /> : <GoEyeClosed size={20} />}
+        </div>
+      )}
     </div>
   );
 };
