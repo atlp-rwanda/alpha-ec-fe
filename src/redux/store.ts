e85ef759ce4596cb68620924987f311f9a1aa18f
IRASUBIZA-Elyse <h.irasubizaelyse@gmail.com>
2024-06-24 15:10:26 +0200

    feat(update password) (#21)
    Users should be able to update the password
    
    [Delivered #187300098]

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -8,6 +8,7 @@ import usersReducer from './slices/disableaccount';
 import profileReducer from './slices/profileSlice';
 import categoriesReducer from './slices/categoriesSlice';
 import itemReducer from './slices/itemSlice';
+import updatePasswordReducer from './slices/updatePasswordSlice';
 
 export const rootReducer = combineReducers({
   otp: otpReducer,
@@ -18,7 +19,8 @@ export const rootReducer = combineReducers({
   forgotPassword: forgotPasswordReducer,
   registereUsers: usersReducer,
   profile: profileReducer,
-  product: itemReducer
+  product: itemReducer,
+  updatePassword: updatePasswordReducer
 });
 
 export const store = configureStore({
