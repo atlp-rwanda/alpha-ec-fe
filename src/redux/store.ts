e0af5fa26eced5bcf59ed34d21a38dff4c9da6d9
M.RWEMA REMY <rwemaremy21@gmail.com>
2024-07-04 10:48:08 +0200

    Authenticated seller can update and delete the product

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -45,7 +45,6 @@ export const rootReducer = combineReducers({
 export const store = configureStore({
   reducer: rootReducer
 });
-
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export default store;
