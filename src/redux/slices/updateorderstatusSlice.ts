3e84d438654da832c95dd6cd7cde6448372e726f
M.RWEMA REMY <rwemaremy21@gmail.com>
2024-07-08 15:09:34 +0200

    the seller can update status
    the seller can update the status and the buyer can get changes

---
+++ src/redux/slices/updateorderstatusSlice.ts
@@ -0,0 +1,77 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { axiosRequest, FormErrorInterface } from '@/utils';
+
+interface UpdateData {
+  orderId: string;
+  newStatus: string;
+}
+
+export interface ProductOrderData {
+  id: string;
+  orderBuyer: {
+    name: string;
+    photoUrl: string | null;
+  };
+  orderedProduct: {
+    images: string[];
+    name: string;
+    price: number;
+  };
+  quantity: number;
+  status: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface ProductOrderState {
+  productOrder: ProductOrderData | null;
+  error: FormErrorInterface | null;
+  loading: boolean;
+}
+
+const initialState: ProductOrderState = {
+  productOrder: null,
+  error: null,
+  loading: false
+};
+
+export const updateProductOrderStatus = createAsyncThunk(
+  'product/updateProductOrderStatus',
+  async (data: UpdateData, { rejectWithValue }) => {
+    try {
+      const response = await axiosRequest(
+        'PUT',
+        `/product-orders/${data.orderId}/status`,
+        { status: data.newStatus },
+        true
+      );
+      return response.data;
+    } catch (error) {
+      return rejectWithValue({ message: 'An error occurred' });
+    }
+  }
+);
+
+const updateProductOrderStatusSlice = createSlice({
+  name: 'productOrderstatus',
+  initialState,
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(updateProductOrderStatus.pending, state => {
+        state.loading = true;
+      })
+      .addCase(updateProductOrderStatus.fulfilled, (state, action) => {
+        state.loading = false;
+        state.productOrder = action.payload;
+        state.error = null;
+      })
+      .addCase(updateProductOrderStatus.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload as FormErrorInterface;
+      });
+  }
+});
+
+export default updateProductOrderStatusSlice.reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -19,6 +19,7 @@ import ordersReducer from './slices/ordersSlice';
 import paymentReducer from './slices/payment';
 import updateprofilereducer from './slices/updateproductSlice';
 import notificationsReducer from './slices/notificationSlice';
+import updateorderstatusreducer from './slices/updateorderstatusSlice';
 
 export const rootReducer = combineReducers({
   otp: otpReducer,
@@ -41,7 +42,8 @@ export const rootReducer = combineReducers({
   orders: ordersReducer,
   payment: paymentReducer,
   updateproduct: updateprofilereducer,
-  notifications: notificationsReducer
+  notifications: notificationsReducer,
+  updateorderstatus: updateorderstatusreducer
 });
 
 export const store = configureStore({
