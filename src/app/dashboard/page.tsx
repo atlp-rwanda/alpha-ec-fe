7cb5998cad13416a4e40054824afabec1035d604
Nadine Fiona BYUKUSENGE <95854257+Nadinefiona@users.noreply.github.com>
2024-07-09 15:16:59 +0200

    fx(sidebar):fixing sidebar & working on logout (#40)

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,17 @@
 'use client';
+import { useEffect } from 'react';
 
 export default function AdminPanel() {
-  return undefined;
+  useEffect(() => {
+    const hasRefreshed = sessionStorage.getItem('hasRefreshed');
+
+    if (!hasRefreshed) {
+      sessionStorage.setItem('hasRefreshed', 'true');
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
+    }
+  }, []);
+
+  return null;
 }
